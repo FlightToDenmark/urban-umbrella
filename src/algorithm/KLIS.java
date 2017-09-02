9ef8d0e9acbeed55be5ca629be97e9f8249e1ce9
Lee Hyeon Jin <shabity@outlook.kr>
2017-09-02 23:17:23 +0900

    KLIS1

--- src/algorithm/KLIS.java
+++ src/algorithm/KLIS.java
@@ -1,9 +1,15 @@
 package algorithm;
 
+import java.util.Scanner;
+
 public class KLIS {
 
 	public static void main(String[] args) {
 
+		Scanner sc = new Scanner(System.in);
+		
+		sc.close();
+		
 	}
 
 }
