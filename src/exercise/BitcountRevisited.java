8f3d0314ee9f35a5b6f271bd28862d63faad7f5b
Lee Hyeon Jin <shabity@outlook.kr>
2017-12-10 20:02:44 +0900

    bitCalculator Data Type 변경 (int -> long -> BigInteger)

--- src/exercise/BitcountRevisited.java
+++ src/exercise/BitcountRevisited.java
@@ -1,5 +1,6 @@
 package exercise;
 
+import java.math.BigInteger;
 import java.util.Scanner;
 
 public class BitcountRevisited {
@@ -11,28 +12,30 @@ public class BitcountRevisited {
 		int testCase = sc.nextInt();
 
 		while (testCase-- > 0) {
-			long from = bitCalculator(sc.nextLong());
-			long to = bitCalculator(sc.nextLong());
-			System.out.println(to - from);
+			BigInteger from = bitCalculator(sc.nextLong());
+			BigInteger to = bitCalculator(sc.nextLong());
+			System.out.println(to.subtract(from));
 		}
-		
+
 		sc.close();
 
 	}
 
-	public static long bitCalculator(long number) {
-		
+	public static BigInteger bitCalculator(long number) {
+
 		String s = Long.toBinaryString(number);
-		
+
 		int length = s.length();
 
-		int total = 0;
-		for(int i=0; i<length; i++) {
-			if(s.charAt(i)=='1') total += (Math.pow(2, length-i)-1);
+		BigInteger total = BigInteger.ZERO;
+		for (int i = 0; i < length; i++) {
+			if (s.charAt(i) == '1') {
+				Long l = (long) (Math.pow(2, length - i) - 1);
+				total = total.add(BigInteger.valueOf(l));
+			}
 		}
-		
-		return total;		
-	
+		return total;
+
 	}
 
 }
