046a9452c038501bffaf873426686c96204f9e9d
Lee Hyeon Jin <shabity@outlook.kr>
2018-01-01 10:22:31 +0900

    Merge branch 'master'

--- src/algospot/TSP2.java
+++ src/algospot/TSP2.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 
 public class TSP2 {
 
+	public static final double INF = 1000000000;
 	public static double[][] distance;
 	public static double[][] cache;
 	public static int C;
@@ -28,7 +29,7 @@ public class TSP2 {
 				for (int j = 0; j < C; j++)
 					distance[i][j] = sc.nextDouble();
 
-			double min = Double.MAX_VALUE;
+			double min = INF;
 			for (int city = 0; city < C; city++) min = Math.min(min, findPaths(city, 1 << city));
 			System.out.format("%.10f", min);
 
@@ -44,7 +45,7 @@ public class TSP2 {
 
 		if (cache[here][visited] >= 0) return cache[here][visited];
 		
-		double ret = Double.MAX_VALUE;
+		double ret = INF;
 		for (int next = 0; next < C; next++) {
 			if ((visited & (1 << next)) != 0) continue;
 			double cand = distance[here][next] + findPaths(next, visited + (1 << next));
