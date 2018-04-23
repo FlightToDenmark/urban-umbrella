a4359d9d55232b098740f51697ac52dc9ed2524a
Lee Hyeon Jin <shabity@outlook.com>
2018-04-23 22:55:32 +0900

    20180422 TLE

--- src/baekjoon/Palindrome13275_TLE.java
+++ src/baekjoon/Palindrome13275_TLE.java
@@ -4,7 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Palindrome13275 {
+public class Palindrome13275_TLE {
 
 	public static void main(String[] args) throws IOException {
 
