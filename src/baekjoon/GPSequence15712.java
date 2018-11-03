8b5b0140f1712fa4440a821798c2258ac5bd4b59
Lee Hyeon Jin <shabity@outlook.com>
2018-11-03 23:01:28 +0900

    분할정복

--- src/baekjoon/GPSequence15712.java
+++ src/baekjoon/GPSequence15712.java
@@ -6,19 +6,52 @@ import java.util.StringTokenizer;
 
 public class GPSequence15712 {
 
+	static long a, r, n, mod;
+
 	public static void main(String[] args) throws Exception {
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
 		StringTokenizer st = new StringTokenizer(br.readLine());
 
-		long a = Long.parseLong(st.nextToken());
-		long r = Long.parseLong(st.nextToken());
-		long n = Long.parseLong(st.nextToken());
-		long mod = Long.parseLong(st.nextToken());
+		a = Long.parseLong(st.nextToken());
+		r = Long.parseLong(st.nextToken());
+		n = Long.parseLong(st.nextToken());
+		mod = Long.parseLong(st.nextToken());
 
-		if (r == 1)	System.out.println(n * a);
-		else System.out.println((a * ((((long) Math.pow(r, n)) - 1) % mod) / (r - 1)));
+		if (mod == 0) {
+			System.out.println("0");
+		} else {
+			System.out.println(a * g(n - 1) % mod);
+		}
+
+	}
+
+	static long power(long a, long b) {
+
+		long sum = 1;
+		while (b > 0) {
+			if (b % 2 == 1) {
+				sum = sum * a % mod;
+			}
+			a = a * a % mod;
+			b /= 2;
+		}
+		return sum;
+
+	}
+
+	static long g(long x) {
+
+		if (x == 0) {
+			return 1;
+		}
+		if (x == 1) {
+			return (1 + r) % mod;
+		}
+		if (x % 2 == 1) {
+			return g(x / 2) * (1 + power(r, x / 2 + 1)) % mod;
+		}
+		return (g(x / 2 - 1) * (1 + power(r, x / 2)) % mod + power(r, x) % mod) % mod;
 
 	}
 
