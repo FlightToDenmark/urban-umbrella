8b576efbaa8e3f6063872b2dfb4cf5d0f032b10d
HyeonJin Lee <shabity@outlook.com>
2019-04-05 14:04:50 +0900

    Create NastyaIsPlayingComputerGames_1136B.java

---
+++ src/cf/NastyaIsPlayingComputerGames_1136B.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int K = Integer.parseInt(st.nextToken());
+		bw.write(3 * N + Math.min(N - K, K - 1) + "\n");
+		bw.close();
+
+	}
+
+}
